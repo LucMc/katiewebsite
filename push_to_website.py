ddf10253b8f914402fb14227aa4abdda9e22e7bf
Katie Cantwell <katiecantwell@outlook.com>
2021-04-09 15:58:42 +0100

    Automated update

--- push_to_website.py
+++ push_to_website.py
@@ -1,5 +1,9 @@
 import os
+'''
+Just run python push_to_website.py
+'''
+
 
 os.system("git add --all")
-os.system('git commit -u "Automated update"')
+os.system('git commit -m "Automated update"')
 os.system("git push")
