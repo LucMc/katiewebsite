9b026930bcaca83947fe21de3092227dd7329f67
Katie Cantwell <katiecantwell@outlook.com>
2021-04-10 08:54:31 +0100

    Automated update

--- push_to_website.py
+++ push_to_website.py
@@ -1,6 +1,14 @@
 import os
 '''
 Just run python push_to_website.py
+Do this to ensure that changes are made on the actual website
+not just the local server
+
+If you want to change any files, images CV etc.
+python manage.py collectstatic
+
+With CV can open in explorer and copy and paste new doc --> make sure
+to rename the same as it was previously (i.e. Katie_CV)
 '''
 
 
